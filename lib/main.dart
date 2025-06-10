7a9ddf71d257b48d883d3c5d339e40cea65a4ddd
Linda <142200607+LindaKampshoff@users.noreply.github.com>
2025-06-10 08:09:18 +0200

    feat(app): added initializeDatabase and Futurebuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,78 @@
+import 'dart:math';
+
+import 'package:drift/drift.dart' show Value;
 import 'package:flutter/material.dart';
+import 'package:lunchify/helper/database_db_helper.dart';
+import 'package:lunchify/models/database.dart';
+import 'package:provider/provider.dart';
+
+import 'helper/modal_helper.dart';
+
+void initializeDatabase(Database db) async {
+  final existingRestaurants = await db.getAllRestaurants();
+  if (existingRestaurants.isEmpty) {
+    final initialRestaurants = [
+      RestaurantsCompanion(
+        id: Value(1),
+        name: Value('Kawaii'),
+        imagePath: Value('assets/images/Kawaii.jpg'),
+      ),
+      RestaurantsCompanion(
+        id: Value(2),
+        name: Value('Roadhouse'),
+        imagePath: Value('assets/images/Roadhouse.jpg'),
+      ),
+      RestaurantsCompanion(
+        id: Value(3),
+        name: Value('Mammas Simple Food'),
+        imagePath: Value('assets/images/Mammas_simple_food.jpg'),
+      ),
+      RestaurantsCompanion(
+        id: Value(4),
+        name: Value('Burger Nerd'),
+        imagePath: Value('assets/images/Burger_Nerd.jpg'),
+      ),
+      RestaurantsCompanion(
+        id: Value(5),
+        name: Value('MC Donald\'s'),
+        imagePath: Value('assets/images/MC_Donalds.jpg'),
+      ),
+      RestaurantsCompanion(
+        id: Value(6),
+        name: Value('Subway'),
+        imagePath: Value('assets/images/Subway.jpg'),
+      ),
+      RestaurantsCompanion(
+        id: Value(7),
+        name: Value('Vesuvio'),
+        imagePath: Value('assets/images/Vesuvio.jpg'),
+      ),
+    ];
+
+    for (var restaurant in initialRestaurants) {
+      await db.insertRestaurant(restaurant);
+    }
+    print('Initial restaurant list loaded into the database.');
+  } else {
+    print('Restaurants already exist in the database.');
+  }
+}
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final db = Database();
+  initializeDatabase(db);
+  runApp(Provider<Database>(create: (_) => db, child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lunchify - Zufallsgenerator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Lunchify'),
@@ -37,16 +82,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,94 +89,160 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> restaurants = [
-    'Restaurant A',
-    'Restaurant B',
-    'Restaurant C',
-    'Restaurant D',
-    'Restaurant E',
-  ];
+  Restaurant? selectedRestaurant;
+  void pickRandom(List<Restaurant> restaurants) {
+    final random = Random();
+    final newPick = restaurants[random.nextInt(restaurants.length)];
+    setState(() {
+      selectedRestaurant = newPick;
+    });
+  }
+
+  Future<void> updateSelectedRestaurantFavoriteStatus() async {
+    if (selectedRestaurant != null) {
+      final updatedRestaurant = await DatabaseHelper().getAllRestaurants();
+      final matchingRestaurant = updatedRestaurant.firstWhere(
+        (restaurant) => restaurant.id == selectedRestaurant!.id,
+        orElse: () => selectedRestaurant!,
+      );
+      setState(() {
+        selectedRestaurant = matchingRestaurant;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("Selected Restaurant: ${selectedRestaurant?.isFavorite}");
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber[100],
+        backgroundColor: Colors.grey[300]!,
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: Column(
-        // Column is also a layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
-        //
-        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-        // action in the IDE, or press "p" in the console), to see the
-        // wireframe for each widget.
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.list, size: 50, color: Colors.grey),
+      body: FutureBuilder(
+        future: DatabaseHelper().getAllRestaurants(),
+        builder: (context, restaurants) {
+          if (restaurants.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (restaurants.hasError) {
+            return Center(
+              child: Text(
+                'Fehler beim Laden der Restaurants: ${restaurants.error}',
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.star, size: 50, color: Colors.grey),
-              ),
-            ],
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 30),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.width * 0.8,
-                  color: Colors.blue,
-                  child: const Center(
-                    child: Placeholder(color: Colors.white, strokeWidth: 2.0),
+            );
+          }
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () async {
+                      if (restaurants.hasData) {
+                        final restaurantsList = restaurants.data!;
+                        if (restaurantsList.isEmpty) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Keine Favoriten vorhanden.'),
+                            ),
+                          );
+                          return;
+                        }
+                        showFavoritesModal(context, restaurantsList);
+                      } else if (restaurants.hasError) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Fehler beim Laden der Restaurants.'),
+                          ),
+                        );
+                      }
+                    },
+                    icon: Icon(Icons.list, size: 50, color: Colors.grey[500]),
                   ),
-                ),
-                Text('${restaurants[1]}'),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0),
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Nochmal',
-                      style: TextStyle(color: Colors.black),
+                  IconButton(
+                    onPressed: () {
+                      if (selectedRestaurant != null) {
+                        DatabaseHelper().toggleFavorite(
+                          selectedRestaurant!.id,
+                          !selectedRestaurant!.isFavorite,
+                        );
+
+                        updateSelectedRestaurantFavoriteStatus();
+                      }
+                    },
+                    icon: Icon(
+                      Icons.star,
+                      size: 50,
+                      color:
+                          selectedRestaurant != null &&
+                                  selectedRestaurant!.isFavorite
+                              ? Colors.amber[100]
+                              : Colors.grey[500],
                     ),
-                    style: ButtonStyle(
-                      padding: WidgetStateProperty.all<EdgeInsets>(
-                        const EdgeInsets.all(16.0),
-                      ),
-                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30),
+                    SizedBox(
+                      width: 500,
+                      child:
+                          selectedRestaurant != null
+                              ? Image(
+                                image: AssetImage(
+                                  selectedRestaurant!.imagePath,
+                                ),
+                              )
+                              : const Center(
+                                child: Placeholder(
+                                  color: Colors.white,
+                                  strokeWidth: 2.0,
+                                ),
+                              ),
+                    ),
+                    selectedRestaurant != null
+                        ? Text(selectedRestaurant!.name)
+                        : Container(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: TextButton(
+                        onPressed: () {
+                          pickRandom(restaurants.data!);
+                        },
+                        style: ButtonStyle(
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                            const EdgeInsets.all(16.0),
+                          ),
+                          shape:
+                              WidgetStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            Colors.amber[100]!,
+                          ),
+                        ),
+                        child: Text(
+                          'Nochmal',
+                          style: TextStyle(color: Colors.black),
                         ),
                       ),
-                      backgroundColor: WidgetStateProperty.all<Color>(
-                        Colors.amber[100]!,
-                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
